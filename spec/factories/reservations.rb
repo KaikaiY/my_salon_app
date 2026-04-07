b897f203a26143b54239e4fb9d3693966d7e25a1
KaikaiY <porter.kai222@gmail.com>
2026-04-07 14:22:24 +0900

    ReservationModelTest

---
+++ spec/factories/reservations.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :reservation do
+    status { :reserved }
+    cancel_reason { nil }
+    note { "テスト予約メモ" }
+    user
+    time_slot
+
+    trait :cancelled do
+      status { :cancelled }
+      cancel_reason { "キャンセル理由" }
+    end
+
+    trait :completed do
+      status { :completed }
+    end
+  end
+end
