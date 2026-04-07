feb7e2f62230536654aa4a22f72368864bb7d37f
KaikaiY <porter.kai222@gmail.com>
2026-04-07 13:31:44 +0900

    ModelTest

---
+++ spec/factories/treatment_days.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :treatment_day do
+    date { Date.current }
+    booking_source { :app }
+    status { :pending }
+    note { "テストメモ" }
+    company
+    association :created_by, factory: %i[user admin]
+    association :therapist, factory: %i[user therapist]
+  end
+end
---
+++ spec/models/treatment_day_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe TreatmentDay, type: :model do
+  describe 'バリデーション' do
+    it '有効なfactoryを持つこと' do
+      expect(build(:treatment_day)).to be_valid
+    end
+
+    it 'date がないと無効であること' do
+      treatment_day = build(:treatment_day, date: nil)
+
+      expect(treatment_day).to be_invalid
+      expect(treatment_day.errors[:date]).to be_present
+    end
+
+    it 'booking_source がないと無効であること' do
+      treatment_day = build(:treatment_day, booking_source: nil)
+
+      expect(treatment_day).to be_invalid
+      expect(treatment_day.errors[:booking_source]).to be_present
+    end
+
+    it 'status がないと無効であること' do
+      treatment_day = build(:treatment_day, status: nil)
+
+      expect(treatment_day).to be_invalid
+      expect(treatment_day.errors[:status]).to be_present
+    end
+
+    it 'therapist がなくても有効であること' do
+      treatment_day = build(:treatment_day, therapist: nil)
+
+      expect(treatment_day).to be_valid
+    end
+    it 'company がないと無効であること' do
+      treatment_day = build(:treatment_day, company: nil)
+
+      expect(treatment_day).to be_invalid
+      expect(treatment_day.errors[:company]).to be_present
+    end
+
+    it 'created_by がないと無効であること' do
+      treatment_day = build(:treatment_day, created_by: nil)
+
+      expect(treatment_day).to be_invalid
+      expect(treatment_day.errors[:created_by]).to be_present
+    end
+
+
+  end
+end
