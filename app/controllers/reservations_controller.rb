f21da311613df5dbc6987bd84d2a0361b6ab1992
KaikaiY <porter.kai222@gmail.com>
2026-04-10 13:47:12 +0900

    ReservationControllerView

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,72 @@
+class ReservationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_employee!, only: %i[new create]
+  before_action :set_time_slot, only: %i[new create]
+  before_action :set_reservation, only: %i[show]
+
+  def index
+    @reservations = reservation_scope.includes(:user, time_slot: :treatment_day).order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @reservation = current_user.reservations.new(time_slot: @time_slot)
+  end
+
+  def create
+    @reservation = current_user.reservations.new(reservation_params.merge(time_slot: @time_slot, status: :reserved))
+
+    if @reservation.save
+      redirect_to reservation_path(@reservation), notice: "予約しました"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def ensure_employee!
+    return if current_user&.employee?
+
+    redirect_to root_path, alert: "権限がありません"
+  end
+
+  def set_time_slot
+    @treatment_day = TreatmentDay.where(company: current_user.company).find_by(id: params[:treatment_day_id])
+
+    unless @treatment_day
+      redirect_to root_path, alert: "予約できない施術日です"
+      return
+    end
+
+    @time_slot = @treatment_day.time_slots.find_by(id: params[:time_slot_id])
+
+    redirect_to root_path, alert: "予約できない時間枠です" unless @time_slot
+  end
+
+  def set_reservation
+    @reservation = reservation_scope.find_by(id: params[:id])
+
+    redirect_to reservations_path, alert: "予約が見つかりません" unless @reservation
+  end
+
+  def reservation_scope
+    return Reservation.all if current_user.admin?
+
+    if current_user.company_manager?
+      return Reservation.joins(time_slot: :treatment_day).where(treatment_days: { company_id: current_user.company_id })
+    end
+
+    if current_user.therapist?
+      return Reservation.joins(time_slot: :treatment_day).where(treatment_days: { therapist_id: current_user.id })
+    end
+
+    current_user.reservations
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:note)
+  end
+end
--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -1,16 +1,24 @@
 class TimeSlotsController < ApplicationController
   before_action :authenticate_user!
-  before_action :ensure_treatment_day_manager!
-  before_action :set_treatment_day
+  before_action :ensure_treatment_day_manager!, except: %i[index]
+  before_action :set_treatment_day, except: %i[index]
   before_action :set_time_slot, only: %i[edit update]
+
+  def index
+    @time_slots = time_slot_scope.includes(treatment_day: %i[company therapist]).order("treatment_days.date ASC", :start_time)
+  end
 
   def new
     @time_slot = @treatment_day.time_slots.new
   end
 
   def create
-    @time_slot = @treatment_day.time_slots.new(time_slot_params)
+    if bulk_create?
+      create_bulk_time_slots
+      return
+    end
 
+    @time_slot = @treatment_day.time_slots.new(time_slot_params)
     if @time_slot.save
       redirect_to treatment_day_path(@treatment_day), notice: "時間枠を登録しました"
     else
@@ -31,6 +39,74 @@ class TimeSlotsController < ApplicationController
 
   private
 
+  def create_bulk_time_slots
+    @time_slot = @treatment_day.time_slots.new(time_slot_params)
+
+    unless @time_slot.valid?
+      render :new, status: :unprocessable_entity
+      return
+    end
+
+    time_slots = build_bulk_time_slots
+
+    if time_slots.empty?
+      @time_slot.errors.add(:base, "20分以上の時間を指定してください")
+      render :new, status: :unprocessable_entity
+      return
+    end
+
+    TimeSlot.transaction do
+      time_slots.each(&:save!)
+    end
+
+    redirect_to treatment_day_path(@treatment_day), notice: "#{time_slots.count}件の時間枠を登録しました"
+  rescue ActiveRecord::RecordInvalid
+    render :new, status: :unprocessable_entity
+  end
+
+  def build_bulk_time_slots
+    interval = 20.minutes
+    current_time = time_slot_datetime(time_slot_params[:start_time])
+    end_time = time_slot_datetime(time_slot_params[:end_time])
+    time_slots = []
+
+    while current_time + interval <= end_time
+      time_slots << @treatment_day.time_slots.new(
+        start_time: current_time,
+        end_time: current_time + interval
+      )
+      current_time += interval
+    end
+
+    time_slots
+  end
+
+  def time_slot_datetime(value)
+    Time.zone.parse(value.to_s)
+  end
+
+  def bulk_create?
+    ActiveModel::Type::Boolean.new.cast(params.dig(:time_slot, :bulk_create))
+  end
+
+  def time_slot_scope
+    scope = TimeSlot.joins(:treatment_day)
+
+    if current_user.admin?
+      scope
+    elsif current_user.company_manager?
+      scope.where(treatment_days: { company_id: current_user.company_id })
+    elsif current_user.therapist?
+      scope.where(treatment_days: { therapist_id: current_user.id })
+    elsif current_user.employee?
+      scope
+        .where(treatment_days: { company_id: current_user.company_id })
+        .where.not(id: Reservation.where.not(status: :cancelled).select(:time_slot_id))
+    else
+      TimeSlot.none
+    end
+  end
+
   def set_treatment_day
     @treatment_day = treatment_day_scope.find(params[:treatment_day_id])
   end
@@ -44,4 +120,3 @@ class TimeSlotsController < ApplicationController
   end
 
 end
-
